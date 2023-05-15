1e4152c94f4b2600b8829be3976385c04ffd54ab
rotem711 <rotemskandrani@gmail.com>
2023-05-15 14:17:24 -0700

    customize question list api

--- src/api/question/controllers/question.ts
+++ src/api/question/controllers/question.ts
@@ -2,6 +2,29 @@
  * question controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::question.question');
+export default factories.createCoreController(
+  "api::question.question",
+  ({ strapi }) => ({
+    // Method 1: Creating an entirely custom action
+    async exampleAction(ctx) {
+      try {
+        ctx.body = "ok";
+      } catch (err) {
+        ctx.body = err;
+      }
+    },
+
+    async find(ctx) {
+      // query only answer text
+      ctx.query = { ...ctx.query, populate: { options: { fields: "text" } } };
+
+      const { data, meta } = await super.find(ctx);
+
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+  })
+);
